6b8c77ed00ae4b4744324927d77b0cdc624d663b
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2022-05-05 17:37:44 +0200

    thermal stuff

--- spin_systems/ClassicalIsingTermalState.py
+++ spin_systems/ClassicalIsingTermalState.py
@@ -18,10 +18,10 @@ def Ising_ThermalState(L, K):
     tensors = [0]*L
     
     sigma_x = np.array([[0,1],[1,0]])
-    
     c_K =  np.cosh(K/2)/np.sqrt(2*np.cosh(K))
     s_K = -np.sinh(K/2)/np.sqrt(2*np.cosh(K))
-    
+    if np.isnan(c_K): c_K = 1./2
+    if np.isnan(s_K): s_K = -1./2*np.sign(K)
     boundL = (np.array([np.eye(2),sigma_x])/np.sqrt(2)).reshape(1,2,2,2)
     bulk   = np.array([[c_K*np.eye(2), c_K * sigma_x],[s_K*sigma_x,s_K*np.eye(2)]]).reshape(2,2,2,2)
     boundR = np.array( [c_K*np.eye(2),  s_K*sigma_x]).reshape(2,2,2,1)
@@ -33,4 +33,6 @@ def Ising_ThermalState(L, K):
     for i in range(L):
         shp = tensors[i].shape
         tensors[i] = tensors[i].reshape(shp[0],shp[1]*shp[2],shp[3])
-    return MPS(L,d=4,tensors=tensors)
+    rho_t = MPS(L,d=4,tensors=tensors)
+    rho_t.right_normalize()
+    return rho_t
