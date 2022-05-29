68301b4d97787835ba2e2d9a9d107f83a9120743
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2022-05-29 16:31:52 +0200

    commit

--- algorithms/iTimeEvolvingBlockDecimation.py
+++ algorithms/iTimeEvolvingBlockDecimation.py
@@ -1,60 +1,61 @@
 import numpy as np
 import opt_einsum as oe
-from ..tensors.MatrixProductState import MPS
-from ..tensors.MatrixProductOperators import MPO
-from ..tools import contract, lanczos
 from ..tools.svd_truncate import svd_truncate
-
-import copy
+from scipy.sparse.linalg import expm
 
 class iTEBD:
-    def __init__(self, A, Sa, B, Sb, U, options={}):
-        """
-        --Sa--A---Sb---B--Sa--
-              |_______|
-              | U(dt) |
-              |͞ ͞ ͞ ͞ ͞ ͞ ͞ |
-              |       |
-        
-        --Sb--B---Sa---A--Sb--
-              |_______|
-              | U(dt) |
-              |͞ ͞ ͞ ͞ ͞ ͞ ͞ |
-              |       |
-
-        Parameters
-        ----------
-        A : np.ndarray
-            first tensor.
-        Sa : np.ndarray
-            singular values.
-        B : np.ndarray
-            second tensor.
-        Sb : np.ndarray
-            singular values.
-        U : np.ndarray
-            two-body potential.
-        options : dict, optional
-            svd_truncate options. The default is {}.
-            
-        """
-        self.A, self.B= A.copy(), B.copy()
-        self.Sa, self.Sb = Sa.copy(), Sb.copy()
-        self.U = U
+    def __init__(self, Sv, B1, B2, H, dt, d=2, options={}):
+        self.B1, self.B2= B1.copy(), B2.copy()
+        self.Sv = Sv.copy()
+        self.d = d
+        self.H = H.reshape(d,d,d,d)
+        self.U = expm(-1j*H*dt).reshape(d,d,d,d).transpose(2,3,0,1)
         self.options = options
         if 'trunc_cut' not in self.options.keys(): self.options.update({'trunc_cut':1e-10})
-        if 'chi_max'   not in self.options.keys(): self.options.update({'chi_max':64})
-        if 'krydim'    not in self.options.keys(): self.options.update({'krydim':10})
+        if 'chi_max'   not in self.options.keys(): self.options.update({'chi_max':128})
+        if 'svd_min'   not in self.options.keys(): self.options.update({'svd_min':1e-10})
         self.truncation_err = 0.
+        self.initial_energy = self.compute_energy()
         
-    def time_step(self, dt):
+    def time_step(self):
         for _ in range(2):
-            shpA, shpB = self.A.shape, self.B.shape
-            theta = oe.contract('a,acd,d,dfg,g,cflm->almg', self.Sa,self.A,self.Sb,self.B,self.Sa,self.U)
-            (U,S,V),err = svd_truncate(theta.reshape(shpA[0]*shpA[1],shpB[1]*shpB[2]), self.options)
-            self.truncation_err = max(err, self.truncation_err)
-            self.Sb = S
-            U = U.reshape(shpA[0],shpA[1],S.size)
-            self.B = V.reshape(S.size,shpB[1],shpB[2])
-            self.A = oe.contract('j,jkl->jkl', 1./self.Sa, U)
-            self.A,self.Sa,self.B,self.Sb = self.B,self.Sb,self.A,self.Sa
+            shp1,shp2 = self.B1.shape,self.B2.shape
+            theta = oe.contract('a,abc,cde,bdfg->afge',self.Sv,self.B1,self.B2,self.U)
+            theta = theta.reshape(shp1[0]*shp1[1],shp2[1]*shp2[2])
+            (U,S,V), err = svd_truncate(theta,self.options)
+            U, V = U.reshape(shp1[0],shp1[1],S.size), V.reshape(S.size,shp2[1],shp2[2])
+            self.truncation_err = max(err,self.truncation_err)
+            self.B2 = V
+            self.B1 = oe.contract('i,ijk,k->ijk',1./self.Sv,U,S)
+            self.Sv, self.B1, self.B2 = S, self.B2, self.B1
+            
+    def err_normalization(self):
+        self.norm = oe.contract('i,ijk,klm,ijn,nlm',self.Sv**2,self.B1,self.B2,self.B1.conj(),self.B2.conj())
+        return np.abs(self.norm-1)
+    
+    def compute_local_observable(self,op):
+        return oe.contract('i,ijk,ilk,jl',self.Sv**2,self.B1,self.B1.conj(),op).item().real
+    
+    def compute_energy(self):
+        return oe.contract('a,abc,cde,afg,ghe,bdfh',self.Sv**2,self.B1,self.B2,self.B1.conj(),self.B2.conj(),self.H).item().real
+    
+    def err_energy(self):
+        return np.abs(self.compute_energy()-self.initial_energy)
+    
+    def compute_corr(self,r,opi,opj=None):
+        corr = np.zeros(r)
+        if opj == None:
+            opj = opi
+        corr[0] = self.compute_local_observable(opi@opj)
+        L = oe.contract('a,abc,ade,bd->ce',self.Sv**2,self.B1,self.B1.conj(),opi)
+        for j in range(1,r):
+            if j % 2 == 0:
+                corr[j] = oe.contract('ab,acd,bed,ce',L,self.B1,self.B1.conj(),opj).item().real
+                L = oe.contract('ab,acd,bcf->df',L,self.B1,self.B1.conj())
+            else:
+                corr[j] = oe.contract('ab,acd,bed,ce',L,self.B2,self.B2.conj(),opj).item().real
+                L = oe.contract('ab,acd,bcf->df',L,self.B2,self.B2.conj())
+        return corr
+            
+            
+        
