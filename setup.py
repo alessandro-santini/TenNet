482c63432293b08e8d7fad8e88fa793aff1e07a0
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2022-09-05 10:48:59 +0200

    Fixed Setup
    pip install TenNet

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ from TenNet import __version__
 setup(
     name='TenNet',
     version=__version__,
-
     url='https://github.com/alessandro-santini/TenNet',
     author='Alessandro Santini',
     author_email='asantini@sissa.it',
-
     py_modules=['TenNet'],
 )
+
